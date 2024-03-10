cc0009e9158420b416e002273d6600aecb59a348
alex <Mr.alexpetrov66@gmail.com>
2024-03-10 22:44:45 +0200

    Added-Lists_Lab

---
+++ Lists_Lab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,17 @@
+﻿List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> result = new List<int>();
+
+
+for (int i = 0; i < nums.Count / 2; i++)
+{
+    int curentSum = nums[i] + nums[nums.Count - 1 - i];
+    result.Add(curentSum);
+}
+
+if (nums.Count % 2 != 0)
+{
+    result.Add(nums[nums.Count/2]);
+}
+Console.WriteLine(string.Join(" ", result));
+
